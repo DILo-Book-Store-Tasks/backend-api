41f92edc50838c8d1a8e897d27a039c339a03505
andringaf <febriana.andringa@gmail.com>
2019-12-09 22:09:22 +0700

    penambahan route user di index

--- index.js
+++ index.js
@@ -32,12 +32,15 @@ require("./db");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const userRouter = require('./src/routers/user')
+
 const app = express();
 const port = 8081;
 
 // Package
 app.use(cors());
 app.use(express.json());
+app.use(userRouter)
 app.use(morgan("dev"));
 
 app.listen(port, () => console.log(`Server Running on Port : ${port}`));
--- src/routers/user.js
+++ src/routers/user.js
@@ -5,15 +5,16 @@ const router = new express.Router()
 const multer = require('multer')
 
 router.post('/users', async (req, res) => {
-  const user = new User(req.body)
+    console.log('masuk')
+//   const user = new User(req.body)
 
-  try {
-    await user.save()
-    const token = await user.generateAuthToken()
-    res.status(201).send({ user, token })
-  } catch (e) {
-    res.status(400).send(e)
-  }
+//   try {
+//     await user.save()
+//     const token = await user.generateAuthToken()
+//     res.status(201).send({ user, token })
+//   } catch (e) {
+//     res.status(400).send(e)
+//   }
 })
 
 router.get('/users', auth, async (req, res) => {
