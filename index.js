df12037d01b63573439dca263c3f7bd0308356e4
andringaf <febriana.andringa@gmail.com>
2019-12-09 20:41:07 +0700

    penambahan folder src

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const morgan = require("morgan");
 const app = express();
 const port = 8081;
 
+const userRouter = require('./src/routers/user')
+
+
 // Package
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(userRouter)
 
 app.listen(port, () => console.log(`Server Running on Port : ${port}`));
