d43baed2c2f46b61e97c274f6465074aa910cbb1
daruzboy <35991575+daruzboy@users.noreply.github.com>
2019-12-09 20:48:19 +0700

    konek ke database

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require("express");
 require("./db");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("./config/keys").mongoURI; //impor key
+mongoose
+    .connect (db, {useNewUrlParser: true})
+    .then (() => console.log("DB Konek"))
+    .catch (err => console.log(err))
+
 
 const app = express();
 const port = 8081;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // Package
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.listen(port, () => console.log(`Server Running on Port : ${port}`));
+
+//lisen server
+app.listen(port, () => console.log(`Server Running on Port : ${port}`))
