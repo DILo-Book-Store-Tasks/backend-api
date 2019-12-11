735001186d472fc802d1de078ca589fe0ddd5c65
andringaf <febriana.andringa@gmail.com>
2019-12-11 17:07:52 +0700

    penambahan multi genre

--- src/models/book.js
+++ src/models/book.js
@@ -1,40 +1,52 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema(
+  {
     book_name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true
     },
-    genre: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-        trim: true,
-        ref: "Genre",
-    },
+    genres: [
+      {
+        genre: {
+          type: mongoose.Types.ObjectId,
+          required: true,
+          ref: "Genre",
+          trim: true
+        }
+      }
+    ],
     author: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true
     },
     desc: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true
+    },
+    cover: {
+      type: String,
+      required: true,
+      trim: true
     },
     qty: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true
     },
     price: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true
     }
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 const Book = mongoose.model("Book", bookSchema);
 
--- src/routers/book.js
+++ src/routers/book.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const Book = require("../models/book");
+const multer = require("multer");
+
 
 const router = new express.Router();
 
-router.post("/books", auth.auth, auth.checkRole, async (req, res, next) => {
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, "./uploads/");
+    },
+    filename: function(req, file, cb) {
+      cb(null, new Date().toISOString() + "-" + file.originalname);
+    }
+  });
+  
+  const imageFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+  
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: imageFilter
+  });
+
+router.post("/books", auth.auth, auth.checkRole ,upload.single("cover"), async (req, res, next) => {
     try {
+        req.body.cover = req.file.path;
         const books = new Book(req.body)
         console.log(books)
 
